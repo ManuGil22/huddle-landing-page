9b2fde1d943994d1b7bbe25cefc5ba5ab66f7d9d
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-08-03 15:01:49 -0300

    brand added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import './App.css';
 // Stylesheets
 import './App.css'
 
+// Images and logos
+import logo from './images/logo.svg'
+
 function App() {
   return (
     <div className="App">
-
+      <div className='main-container'>
+        <img src={logo} alt='logo'></img>
+      </div>
     </div>
   );
 }
