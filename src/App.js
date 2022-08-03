5e6e524cad3d1c7e2aaba5b512f01b3310513687
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-08-03 15:45:46 -0300

    social media icons added

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import './App.css'
 // Images and logos
 import logo from './images/logo.svg'
 import design from './images/illustration-mockups.svg'
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 function App() {
   return (
     <div className="App">
       <div className='app-container'>
+
         <img src={logo} alt='logo'></img>
+
         <div className='main-container'>
           <img className='design' src={design} alt='design'></img>
           <div className='description-container'>
@@ -22,6 +25,12 @@ function App() {
              Create connections with your users as you engage in genuine discussion.</p>
             <button className='btn'>Register</button>
           </div>
+        </div>
+
+        <div className='icons-container'>
+          <div className='icon-circle'><FaFacebookF className='icon' color='white' /></div>
+          <div className='icon-circle'><FaTwitter className='icon' color='white' /></div>
+          <div className='icon-circle'><FaInstagram className='icon' color='white' /></div>
         </div>
       </div>
     </div>
